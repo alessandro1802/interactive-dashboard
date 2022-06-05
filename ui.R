e3089afd1c727a09d6c807fa463a017b63f2ef1b
alessandro <vita02051802@gmail.com>
2022-06-05 11:27:16 +0200

    uploaded dashboard template

---
+++ ui.R
@@ -0,0 +1,39 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+
+# Define UI for application that draws a histogram
+shinyUI(
+  fluidPage(
+      # Application title
+      titlePanel("Title"),
+    
+    tabsetPanel(
+      # Tab 1
+      tabPanel("tab 1", 
+               sliderInput("bins", 
+                           "Number of bins:", 
+                           min = 1, 
+                           max = 50, 
+                           value = 30),
+               plotOutput("distPlot"),
+      ),
+      # Tab 2
+      tabPanel("tab 2",
+               "contents"
+      ),
+      # Tab 3
+      tabPanel("tab 3",
+               "contents"
+      )
+    )
+  )
+)
