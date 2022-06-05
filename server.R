d2dd61ed595fc955841bce66d3edf70eb6de9ef2
alessandro <vita02051802@gmail.com>
2022-06-05 21:33:25 +0200

    added 1st visualization and refined ui

--- server.R
+++ server.R
@@ -1,25 +1,65 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(shinydashboard)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+# Load vag.csv
+vag <- read.csv(file = 'data/vag.csv')
+vag$brand <- factor(vag$brand, levels = vag$brand)
+vag$category <- factor(vag$category, levels = c("Volume", "Premium", "Sport"))
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
+shinyServer(function(input, output, session) {
+      ### Main tab
+      global <- reactiveValues(
+        toHighlight = rep(FALSE, length(vag$brand)), 
+        selectedBarDesc = "(select a Brand by clicking on its bar)"
+      )
+      
+      observeEvent(eventExpr = input$barplot_click, {
+        global$selectedBarDesc <- vag$description[round(input$barplot_click$x)]
+        global$toHighlight <- vag$description %in% global$selectedBarDesc
+      })
+      
+      output$barplot <- renderPlot({
+        ggplot(data = vag, aes(x = brand,
+                               y = price,
+                               color = category,
+                               fill = ifelse(global$toHighlight, yes = "yes", no = "no"))) +
+          geom_bar(stat="identity", size = 0.6) +
+          scale_y_continuous(breaks = append(seq(0, 80, by = 10), 5, 1)) +
+          # colours
+          scale_fill_manual(values = c("yes" = "#92c5de", "no" = "#d1e5f0" ), guide = "none" ) +
+          scale_color_manual(values=c("#313695", "#fdae61", "#d73027")) +
+          # titles
+          # labs(title = "VAG revenue in 2021 by brand", x = "Brand", y = "Billion euros", color = "Category") +
+          labs(x = "Brand", y = "Billion euros", color = "Category") +
+          # theme
+          theme_minimal() +
+          # font sizes
+          theme(axis.text = element_text(size = 14),
+                axis.title = element_text(size = 18),
+                plot.title = element_text(size = 22),
+          # grid
+                panel.grid.major.x = element_blank() ,
+                panel.grid.major.y = element_line(size = .1, color = "black"), 
+          # legend
+                legend.title = element_text(size = 15, face = "bold"), 
+                legend.text=element_text(size = 13), 
+                legend.position="top") + 
+          guides(color = guide_legend(override.aes = list(fill = "#d1e5f0")))
+      })
+      
+      output$brandDescription <- renderText({ 
+        paste(strong("Description: "), p(), global$selectedBarDesc)
+      })
+      
+      ### Tab 7
+      # generate bins based on input$bins from ui.R
+      output$distPlot <- renderPlot({
         x    <- faithful[, 2]
         bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
+  
         # draw the histogram with the specified number of bins
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+      })
 
 })
--- ui.R
+++ ui.R
@@ -1,17 +1,7 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(shinydashboard)
 library(shinythemes)
 
-# Define UI for application that draws a histogram
 shinyUI(
   fluidPage(
     # Theme
@@ -19,12 +9,12 @@ shinyUI(
     # Application title
     titlePanel(
       div(
-        "Title",
+        "Volkswagen Group observations and statistics",
         # Logo
-        img(src="logo.png", 
+        img(src="logo1.png", 
               align = "right", 
-              width="30%", 
-              height="30%", 
+              width="20%", 
+              height="20%", 
               style="vertical-align: top;"
         )
       )
@@ -32,22 +22,80 @@ shinyUI(
     # Tab panels
     tabsetPanel(
       # Tab 1
-      tabPanel("Main", 
-               "Some info ..."
+      tabPanel("About",
+               p("This page is about Volkswagen Group."),
+               p("
+                 The Volkswagen Group sells passenger cars under the Audi, Bentley, Cupra, Lamborghini, Porsche, SEAT, Škoda and Volkswagen brands; motorcycles under the Ducati name;
+                 light commercial vehicles under the Volkswagen Commercial Vehicles brand; and heavy commercial vehicles via the marques of listed subsidiary Traton 
+                 (IC Bus, International, MAN, Scania and Volkswagen Caminhões e Ônibus). It is divided into two primary divisions—the Automotive Division and the 
+                 Financial Services Division—and as of 2008, it had about 342 subsidiary companies. Volkswagen also has two major joint ventures in China, FAW-Volkswagen and SAIC Volkswagen. 
+                 The company has operations in roughly 150 countries, and it has 100 production facilities across 27 countries.
+                 "), 
+               p("
+                 From the late 1970s to 1992, the acronym V.A.G. was used by Volkswagen AG as a brand for group-wide activities, such as distribution and leasing. Contrary to popular belief, 
+                 'V.A.G.' had no official meaning, and was never the formal name of the Volkswagen Group. 
+                 "),
+               p("You can find here interesting facts, statistics and comparisons of different car models."), 
+               p(a("Source: Wikipedea page of Volkswagen Group", 
+                   href="https://en.wikipedia.org/wiki/Volkswagen_Group"), 
+                 align = "center")
       ),
+      
       # Tab 2
-      tabPanel("tab 2",
-               sliderInput("bins", 
-                           "Number of bins:", 
-                           min = 1, 
-                           max = 50, 
-                           value = 30),
-               plotOutput("distPlot"),
+      tabPanel("VAG",
+               p("
+                  The Volkswagen Group (Volkswagen Aktiengesellschaft) focuses on producing automobiles, commercial vehicles, engines, motorcycles, and turbo-machinery.
+                  The company, which is publicly listed on the Frankfurt Stock Exchange (under the stock ticker symbol ‘VOW’), is based in Wolfsburg, Germany.
+                  It is one of the world’s leading producers of motor vehicles (along with General Motors and Toyota). It is currently the second largest automaker in the world, after Toyota.
+                  In July 2014, it pushed GM into third place. Analysts believe the company will take the number 1 spot within the next 18 months.
+                  The group sells its vehicles in 153 countries and has a total of 106 production plants across Europe, the Americas, Asia and Africa.
+                  Volkswagen claims that its team of 572,800 employees produces approximately 39,350 vehicles on a regular weekday.
+                 "),
+               p("
+                  The plot below shows VAG revenue in 2021 by brand. 
+                  Clicking on a particular bar displays the information about the manufacturer.
+                 "),
+               p(), 
+               fluidRow(
+                 column(8,
+                        plotOutput("barplot", click = "barplot_click")
+                 ),
+                 column(4,
+                        htmlOutput("brandDescription")
+                 )
+               )
       ),
+      
       # Tab 3
-      tabPanel("tab 3",
+      tabPanel("tab",
                "contents"
+      ), 
+      
+      # Tab 4
+      tabPanel("tab",
+               "contents"
+      ), 
+      
+      # Tab 5
+      tabPanel("tab",
+               "contents"
+      ), 
+      
+      # Tab 6
+      tabPanel("tab",
+               "contents"
+      ), 
+      
+      # Tab 7
+      tabPanel("Test",
+               sliderInput("bins",
+                           "Number of bins:",
+                           min = 1,
+                           max = 50,
+                           value = 30),
+               plotOutput("distPlot")
       )
+      
     )
   )
 )
